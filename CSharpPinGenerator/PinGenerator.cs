2c8f4d3f15d743d4c4e37add1d4db4f966007fe6
Patrick_Desktop <paddymcpherson@hotmail.com>
2021-02-25 16:19:34 +0000

    Add project files.

---
+++ CSharpPinGenerator/PinGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharpPinGenerator
+{
+    public class PinGenerator
+    {
+        private readonly Random _random = new Random();
+
+        public string GenPin()
+        {
+         
+            int pin = _random.Next(10000);
+            string stringPin = pin.ToString();
+            return stringPin;
+        }
+    }
+}
---
+++ CSharpPinGenerator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace CSharpPinGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pinClass = new PinClass();
+            var pinGenerator = new PinGenerator();
+
+            // for when it needs to run once, use the date offset as the key value in the hashtable 
+            for (long i = 0; i < 9980; i++)
+            {
+                string pin = pinGenerator.GenPin();
+                string pinPadded = pin.PadLeft(4, '0');
+                while (pinClass.pinsNotToGen.ContainsValue(pinPadded) || pinClass.PinsAlreadyGenned.ContainsValue(pinPadded))
+                {
+                    pin = pinGenerator.GenPin();
+                    pinPadded = pin.PadLeft(4, '0');
+                }
+
+                if (Int32.Parse(pinPadded) < 1000)
+                {
+                    pinClass.PinsAlreadyGenned.Add(i, pinPadded);
+                    Console.WriteLine("Number: {0}",pinPadded);
+                }
+                else
+                {
+                    pinClass.PinsAlreadyGenned.Add(i,pinPadded);
+                    Console.WriteLine("Number: {0}",pinPadded);
+                }
+            }
+            Console.WriteLine(pinClass.PinsAlreadyGenned.Count);
+        }
+    }
+}
